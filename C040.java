7768950c786d89baef427dc342de727cabca5dfa
simasiman <34100632+simasiman@users.noreply.github.com>
2017-12-05 23:25:56 +0900

    Update and rename C040 to C040.java

--- C040.java
+++ C040.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class C040 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = Integer.parseInt(sc.nextLine());
