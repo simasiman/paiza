1aa75eb23e03c3c03fc50bbb08eba3b16aa62475
simasiman <34100632+simasiman@users.noreply.github.com>
2017-12-05 23:30:24 +0900

    Update and rename S_rensyu1 to S_rensyu1.java

--- S_rensyu1.java
+++ S_rensyu1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class S_rensyu1 {
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String line = sc.nextLine();
