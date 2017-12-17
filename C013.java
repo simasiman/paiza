546e915aca41f166eb4ea1f925278843586e1cdf
simasiman <34100632+simasiman@users.noreply.github.com>
2017-12-18 07:48:17 +0900

    Update C013.java

--- C013.java
+++ C013.java
@@ -1,33 +1,29 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-public class C013 {
+public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		sc.useDelimiter("\n");
 		String n = sc.nextLine();
 		char c = n.charAt(0);
 		int m = Integer.parseInt(sc.nextLine());
-		String[] ary = new String[m];
 		int count = 0;
 
 		ArrayList<String> result = new ArrayList<String>();
 
 		for(int i=0; i<m; i++) {
-			String str = sc.nextLine();
-			ary[i] = str;
-		}
-
-		for(int i=0; i<ary.length; i++) {
-			for(int j=0;j<ary[i].length();j++) {
-				if(ary[i].charAt(j) == c) {
+	 		String str = sc.nextLine();
+			for(int j=0;j<str.length();j++) {
+				if(str.charAt(j) == c) {
 					count++;
 				}
 			}
 			if(count == 0) {
-				result.add(ary[i]);
+				result.add(str);
 			}
 			count = 0;
 		}
+
 		if(result.isEmpty()) {
 			System.out.println("none");
 		} else {
